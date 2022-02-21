f6fa4a7adaab5a982e98d2564a800ba7dc5a85c0
nurgul <nurgulakhsh@gmail.com>
2022-02-21 14:38:06 -0700

    resolves conflicts

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 const controller = require('../controllers/controller')
-const services = require('')
+const services = require('../services/render')
 
-routes.get('/', services)
+routes.get('/', services.home_route)
 routes.route('/api/users').post(controller.create)
 
+module.exports = routes
--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 exports.home_route = (req, res) => {
-    axios.get('http://localhost:3000/api/users')
+    axios.get('http://localhost:3000/')
     .then(function(response) {
         res.render('index', {users: response.data.data})
     }).catch(err=> {
