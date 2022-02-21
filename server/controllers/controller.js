6ebad2092d35558404175f3833dd112fb6d33e70
nurgul <nurgulakhsh@gmail.com>
2022-02-21 13:39:38 -0700

    sends code errors when creating user

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,7 +2,20 @@ const UserModel = require('../models/UserModel')
 
 exports.create = async (req, res) => {
     if (!req.body) {
-        console.log('Can not be empty')
+        res.status(400).json({
+            status: "fail",
+            message: "Body can not be empty"
+        })
     }
 
+    user = await new UserModel({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    user.save().then(data => {
+        res.send(200).json({
+            message: "User added"
+        })
+    })
 }
