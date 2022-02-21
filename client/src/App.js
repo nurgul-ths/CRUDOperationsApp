87533f3a87d95c3c5b1cb5247c1d51a5c9fa531c
nurgul <nurgulakhsh@gmail.com>
2022-02-21 15:04:36 -0700

    change font in header

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,31 @@ function App() {
             <header className='App-header'>
                 Application
             </header>
+            <div class="container">
+        <div class="box-justify-between">
+            <a href="/add-user" class="border-shadow">
+                <span class="text-main"> New User </span>
+            </a>
+        </div>
+
+        <form action='/' method='POST'>
+            <table style="width:100%">
+                <thead class="thead">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th> 
+                        <th>Status</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <%- include('include/_show')%>
+                </tbody>
+              </table>
+        </form>
+        
+    </div>
         </div>
     )
 }
